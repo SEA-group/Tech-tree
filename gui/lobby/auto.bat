c04fb8ab6c9ef18f70e164a855d60d111dc742c9
diannao <55k@outlook.com>
2016-12-01 12:06:32 +0900

    0515pt o

---
+++ gui/lobby/auto.bat
@@ -0,0 +1,8 @@
+@echo off
+copy /y /v ".\shipstree.xml" ".\shipstree_ru.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_cn.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_na.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_asia.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_eu.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_pt.xml"
+copy /y /v ".\shipstree.xml" ".\shipstree_st.xml"
